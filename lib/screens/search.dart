a859973ba6eedde6407b8e5ee6a3503b496faf68
ilikesunday <dhanushnatra7@gmail.com>
2025-02-26 14:23:00 +0530

    changed tab bar colors and added icons for song tile

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -8,13 +8,34 @@ class SearchScreen extends StatefulWidget {
   State<SearchScreen> createState() => _SearchScreenState();
 }
 
-class _SearchScreenState extends State<SearchScreen> {
+class _SearchScreenState extends State<SearchScreen>
+    with SingleTickerProviderStateMixin {
   final TextEditingController _controller = TextEditingController();
   bool submit = false;
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+  }
+
+  void tabchange(int index) {
+    setState(() {
+      _tabController.index = index;
+    });
+  }
+
   void onsubmit() {
     setState(() {
       submit = true;
     });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
@@ -36,7 +57,37 @@ class _SearchScreenState extends State<SearchScreen> {
           buildSearchBar(),
           submit
               ? Flexible(
-                child: DefaultTabController(length: 4, child: buildTabBar()),
+                child: DefaultTabController(
+                  length: 4,
+                  child: Column(
+                    children: [
+                      TabBar(
+                        controller: _tabController,
+                        tabs: [
+                          Tab(text: "Songs"),
+                          Tab(text: "Albums"),
+                          Tab(text: "Artists"),
+                          Tab(text: "Playlists"),
+                        ],
+                        indicatorColor: Colors.green,
+                        labelColor: Colors.green,
+                        unselectedLabelColor: Colors.white,
+                      ),
+                      SizedBox(height: 10),
+                      Expanded(
+                        child: TabBarView(
+                          controller: _tabController,
+                          children: [
+                            songresults(_controller.text),
+                            albumresults(_controller.text),
+                            artistresults(_controller.text),
+                            playlistresults(_controller.text),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               )
               : Container(),
         ],
@@ -44,28 +95,23 @@ class _SearchScreenState extends State<SearchScreen> {
     );
   }
 
-  Widget buildTabBar() {
-    return Column(
-      children: [
-        const TabBar(
-          tabs: [
-            Tab(text: "Songs"),
-            Tab(text: "Albums"),
-            Tab(text: "Artists"),
-            Tab(text: "Playlists"),
-          ],
-        ),
-        Expanded(
-          child: TabBarView(
-            children: [
-              songresults(_controller.text),
-              albumresults(_controller.text),
-              artistresults(_controller.text),
-              playlistresults(_controller.text),
-            ],
+  Widget buildSearchBar() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: TextField(
+        controller: _controller,
+        onSubmitted: (value) => onsubmit(),
+        decoration: InputDecoration(
+          hintText: "Search",
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide: BorderSide.none,
           ),
+          filled: true,
+          fillColor: Colors.green.shade900,
+          prefixIcon: const Icon(Icons.search),
         ),
-      ],
+      ),
     );
   }
 
@@ -80,12 +126,10 @@ class _SearchScreenState extends State<SearchScreen> {
         } else if (!snapshot.hasData || snapshot.data!.artists.isEmpty) {
           return const Center(child: Text('No results found'));
         } else {
-          return Expanded(
-            child: ListView.builder(
-              itemCount: snapshot.data!.artists.length,
-              itemBuilder:
-                  (context, index) => artisttile(snapshot.data!.artists[index]),
-            ),
+          return ListView.builder(
+            itemCount: snapshot.data!.artists.length,
+            itemBuilder:
+                (context, index) => artisttile(snapshot.data!.artists[index]),
           );
         }
       },
@@ -116,13 +160,11 @@ class _SearchScreenState extends State<SearchScreen> {
         } else if (!snapshot.hasData || snapshot.data!.playlists.isEmpty) {
           return const Center(child: Text('No results found'));
         } else {
-          return Expanded(
-            child: ListView.builder(
-              itemCount: snapshot.data!.playlists.length,
-              itemBuilder:
-                  (context, index) =>
-                      playlisttile(snapshot.data!.playlists[index]),
-            ),
+          return ListView.builder(
+            itemCount: snapshot.data!.playlists.length,
+            itemBuilder:
+                (context, index) =>
+                    playlisttile(snapshot.data!.playlists[index]),
           );
         }
       },
@@ -145,26 +187,6 @@ class _SearchScreenState extends State<SearchScreen> {
     );
   }
 
-  Widget buildSearchBar() {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      child: TextField(
-        controller: _controller,
-        onSubmitted: (value) => onsubmit(),
-        decoration: InputDecoration(
-          hintText: "Search",
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(20),
-            borderSide: BorderSide.none,
-          ),
-          filled: true,
-          fillColor: Colors.green.shade900,
-          prefixIcon: const Icon(Icons.search),
-        ),
-      ),
-    );
-  }
-
   Widget albumresults(query) {
     return FutureBuilder<Albums>(
       future: SaavnApi().albums.fetchAlbums(query, n: "20"),
@@ -176,12 +198,10 @@ class _SearchScreenState extends State<SearchScreen> {
         } else if (!snapshot.hasData || snapshot.data!.albums.isEmpty) {
           return const Center(child: Text('No results found'));
         } else {
-          return Expanded(
-            child: ListView.builder(
-              itemCount: snapshot.data!.albums.length,
-              itemBuilder:
-                  (context, index) => albumtile(snapshot.data!.albums[index]),
-            ),
+          return ListView.builder(
+            itemCount: snapshot.data!.albums.length,
+            itemBuilder:
+                (context, index) => albumtile(snapshot.data!.albums[index]),
           );
         }
       },
@@ -213,15 +233,28 @@ class _SearchScreenState extends State<SearchScreen> {
         } else if (!snapshot.hasData || snapshot.data!.songs.isEmpty) {
           return const Center(child: Text('No results found'));
         } else {
-          return Expanded(
-            child: ListView.builder(
-              itemCount: snapshot.data!.songs.length,
-              itemBuilder:
-                  (context, index) => songtile(snapshot.data!.songs[index]),
-            ),
+          return ListView.builder(
+            itemCount: snapshot.data!.songs.length,
+            itemBuilder:
+                (context, index) => songtile(snapshot.data!.songs[index]),
           );
         }
       },
+    );
+  }
+
+  Widget songAlert() {
+    return AlertDialog(
+      title: Text("Song"),
+      content: Text("Song"),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text("Close"),
+        ),
+      ],
     );
   }
 
@@ -236,6 +269,70 @@ class _SearchScreenState extends State<SearchScreen> {
           child: Image.network(song.imageUrl),
         ),
       ),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          IconButton(
+            icon: Icon(
+              Icons.favorite_border_outlined,
+              color: Colors.green.shade500,
+            ),
+            onPressed: () {
+              // audioController.playTrack(song.url, song.title);
+              print("adding to fav ${song.title}");
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert_rounded, color: Colors.green.shade500),
+            onPressed: () {
+              // audioController.playTrack(song.url, song.title);
+              final RenderBox overlay =
+                  Overlay.of(context).context.findRenderObject() as RenderBox;
+              showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return Container(
+                    height: 200,
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Text("Add to Playlist"),
+                          onTap: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => songAlert(),
+                            );
+                          },
+                        ),
+                        ListTile(
+                          title: Text("Share"),
+                          onTap: () {
+                            print("sharing ${song.title}");
+                          },
+                        ),
+                        ListTile(
+                          title: Text("More"),
+                          onTap: () {
+                            print("more ${song.title}");
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+              print("more ${song.title}");
+            },
+          ),
+        ],
+      ),
+      onLongPress:
+          () => showDialog(context: context, builder: (context) => songAlert()),
+      onTap: () {
+        // audioController.playTrack(song.url, song.title);
+        print("playing ${song.title}");
+      },
     );
   }
 }
